bc300f995edb20d2df32b22a8ba012a1f31408f9
akshgupta025-sketch <akshgupta025@gmail.com>
2026-03-05 17:24:41 +0530

    Time: 184 ms (8.13%), Space: 53.7 MB (5.4%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,29 +3,31 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
-       set<vector<int>>st;
-       vector<vector<int>>ans;
-       for(int i=0;i<n-3;i++){
-        for(int j=i+1;j<n-2;j++){
-            long long newt = 1LL*target - nums[i]-nums[j];
-            int low = j+1;
-            int high = n-1;
-            while(low<high){
-             long long sum =  1LL*nums[low]+nums[high];
-                if(sum<newt){
-                    low++;
-                } else if(sum>newt){
-                    high--;
-                } else{
-                    st.insert({nums[i],nums[j],nums[low],nums[high]});
-                        low++; high--;
+        set<vector<int>>st;
+        vector<vector<int>>ans;
+        for(int i =0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+                long long newtarget = 1LL*target-nums[i]-nums[j];
+                int low = j+1;
+                int high = n-1;
+                while(low<high){
+                    long long sum = 1LL*nums[low]+nums[high];
+                    if(sum<newtarget){
+                        low++;
+                    }
+                    else if(sum>newtarget){
+                        high--;
+                    } else{
+                        st.insert({nums[i],nums[j],nums[low],nums[high]});
+                        low++;
+                        high--;
                     }
                 }
             }
         }
-       for(auto it:st){
-        ans.push_back(it);
-       }
-       return ans;
+        for(auto it:st){
+            ans.push_back(it);
+        }
+        return ans;
     }
 };
