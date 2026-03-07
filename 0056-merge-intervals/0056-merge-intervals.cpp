e91740ccfdae5183da6c2a646a5bd4ce7c221732
akshgupta025-sketch <akshgupta025@gmail.com>
2026-03-07 23:44:30 +0530

    Time: 4 ms (66.81%), Space: 23.9 MB (59.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,17 +5,18 @@ public:
         vector<vector<int>> ans;
         int n = intervals.size();
 
-        for(int i = 0; i < n; i++) {
-
-            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
-                ans.push_back(intervals[i]);
-            }
-            else {
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-            }
-
+        for(int i = 0; i < n;) {
+          
+          int start = intervals[i][0];
+          int end = intervals[i][1];
+          int j = i+1;
+          while(j<n && intervals[j][0]<=end){
+            end = max(end,intervals[j][1]);
+            j++;
+    }
+    ans.push_back({start,end});
+    i=j;
         }
-
         return ans;
     }
 };
