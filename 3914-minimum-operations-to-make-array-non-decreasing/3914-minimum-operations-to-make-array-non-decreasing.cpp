a94bec0f119175f832f7d24f6a5cbdf92568338e
Akshat Gupta <akshgupta025@gmail.com>
2026-04-26 09:42:09 +0530

    Time: 0 ms (100%), Space: 136.9 MB (34.15%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -3,14 +3,12 @@ public:
     using ll = long long;
     long long minOperations(vector<int>& nums) {
         int n = nums.size();
-        ll t = 0;
         ll a = 0;
         for(int i=0;i+1<n;i++){
-            ll p = nums[i]+t;
-            ll q = nums[i+1]+t;
+            ll p = nums[i]+a;
+            ll q = nums[i+1]+a;
             if(q<p){
               ll c = p-q;
-                t+=c;
                 a+=c;
             }
         }
