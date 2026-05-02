a75bc344efe83224efee6a8705208cdc561c633b
Akshat Gupta <akshgupta025@gmail.com>
2026-05-02 13:14:55 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.74%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.size()!=t.size()){
+        return false;
+       }
+       vector<int>freq(26,0);
+       for(auto c:s){
+        freq[c-'a']++;
+       }
+       for(auto c:t){
+        freq[c-'a']--;
+       }
+       for(auto i:freq){
+        if(i!=0) return false;
+       }
+       return true;
+    }
+};
