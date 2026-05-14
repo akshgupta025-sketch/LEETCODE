f37d79db069c9a477d8105c01f01568625014a9f
Akshat Gupta <akshgupta025@gmail.com>
2026-05-14 10:45:39 +0530

    Time: 8 ms (83.46%), Space: 11.9 MB (51.83%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_map<ListNode*,int>mp;
-        while(temp){
-            if(mp.find(temp)!=mp.end()){
+        // APPROACH -1 STORING VISITING NODES IN MAP AND CHECKING 
+        // ListNode* temp = head;
+        // unordered_map<ListNode*,int>mp;
+        // while(temp){
+        //     if(mp.find(temp)!=mp.end()){ 
+        //         return true;
+        //     }else{
+        //         mp[temp]=1;
+        //         temp=temp->next;
+        //     }
+        // }
+        // return false;
+        // APPROACH -2 TORTOISE HARE ALGO SLOW AND FAST
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow){
                 return true;
-            }else{
-                mp[temp]=1;
-                temp=temp->next;
             }
         }
-        return false;
+        return false;  
     }
 };
