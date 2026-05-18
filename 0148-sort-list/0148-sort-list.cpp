2615d215438056c86891b4dbba91e07f13b01789
Akshat Gupta <akshgupta025@gmail.com>
2026-05-18 12:38:11 +0530

    Time: 52 ms (19.93%), Space: 75.6 MB (39.12%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,24 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        if(head==NULL||head->next==NULL){
-            return head;
+ListNode* middle(ListNode*head){
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+ListNode* merge(ListNode* list1,ListNode* list2){
+    ListNode* dummynode = new ListNode(-1);
+    ListNode* temp = dummynode;
+    while(list1!=NULL && list2!=NULL){
+        if(list1->val>list2->val){
+            temp->next=list2;
+            list2 = list2->next;
+        }else{
+            temp->next=list1;
+            list1 = list1->next;
         }
-        vector<int>a;
-        ListNode* temp = head;
-        while(temp){
-            a.push_back(temp->val);
-            temp=temp->next;
-        }
-        sort(a.begin(),a.end());
-        temp = head;
-        int i = 0;
-        while(temp){
-           temp->val = a[i];
-           i++;
-           temp=temp->next; 
-        }
+        temp = temp->next;
+    }
+    if(list1) {temp->next = list1;
+    }
+    else {temp->next = list2;
+    }
+    return dummynode->next;
+}
+ListNode* recurs(ListNode* head){
+    if(head==NULL||head->next==NULL){
         return head;
+    }
+    ListNode* mid = middle(head);
+     ListNode* lefthead = head;
+     ListNode* righthead = mid->next;
+      mid->next = NULL;
+     lefthead = recurs(lefthead);
+     righthead = recurs(righthead);
+     return merge(lefthead,righthead);
+}
+    ListNode* sortList(ListNode* head) {
+      return recurs(head);  
     }
 };
