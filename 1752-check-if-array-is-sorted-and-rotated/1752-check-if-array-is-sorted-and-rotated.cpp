6ea07186c5e29181341fce1780bed3edee35f9f1
Akshat Gupta <akshgupta025@gmail.com>
2026-05-23 11:40:37 +0530

    Time: 2 ms (3.21%), Space: 13.9 MB (5.2%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -10,27 +10,26 @@ public:
   }
     bool check(vector<int>& nums) {
         // NOT OPTIMAL
-    //    int n = nums.size();
-    //    for(int k = 0;k<n;k++){
-    //     vector<int>temp(n);
-    //     for(int i=0;i<n;i++){
-    //         temp[(i+k)%n]=nums[i];
-    //     }
-    //     if(is(temp)){
-    //         return true;
-    //    }
-    // }
-    // return false;
-    // }
-    // LOGIC - SORTED COMPARE ADJACENT + ROTATION PROPERTY - OBSERV ROTATE > <=1 ->TRUE
-    int n = nums.size();
-    int cnt = 0;
-    for(int i=0;i<n;i++){
-        if(nums[(i+1)%n]<nums[i]){
-            cnt++;
+       int n = nums.size();
+       for(int k = 0;k<n;k++){
+        vector<int>temp(n);
+        for(int i=0;i<n;i++){
+            temp[(i+k)%n]=nums[i];
         }
+        if(is(temp)){
+            return true;
+       }
     }
-    return cnt<=1;
+    return false;
     }
+    // LOGIC - SORTED COMPARE ADJACENT + ROTATION PROPERTY - OBSERV ROTATE > <=1 ->TRUE
+    // int n = nums.size();
+    // int cnt = 0;
+    // for(int i=0;i<n;i++){
+    //     if(nums[(i+1)%n]<nums[i]){
+    //         cnt++;
+    //     }
+    // }
+    // return cnt<=1;
 };
     
