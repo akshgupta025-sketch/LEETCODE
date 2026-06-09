a613a9764411b60538e48f51e597a74801bc5a8c
Akshat Gupta <akshgupta025@gmail.com>
2026-06-09 23:34:35 +0530

    Time: 2 ms (72.48%), Space: 10.1 MB (77.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,21 @@
 class Solution {
 public:
-    bool is_exist(int row,int col, vector<string>& board,int n){
-       int duprow = row;
-       int dupcol = col;
-       while(row>=0 && col>=0){
-        if(board[row][col]=='Q')return false;
-        row--;
-        col--;
-       }
-       row = duprow;
-       col = dupcol;
-       while(col>=0){
-        if(board[row][col]=='Q')return false;
-        col--;
-       }
-       row = duprow;
-       col = dupcol;
-       while(row<n && col>=0){
-        if(board[row][col]=='Q')return false;
-        row++;
-        col--;
-       }
-       return true;
-    }
-    void recur(int col,vector<vector<string>>& ans,vector<string>& board,int n){
+    void recur(int col,vector<vector<string>>& ans,vector<string>& board,vector<int>& leftrow,vector<int>& lowerdiag,vector<int>& upperdiag,int n){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row = 0;row<n;row++){
-            if(is_exist(row,col,board,n)){
+            if(leftrow[row]==0 && lowerdiag[row+col]==0 && upperdiag[n-1+col-row]==0){
                 board[row][col]='Q';
-                recur(col+1,ans,board,n);
+                leftrow[row]=1;
+                lowerdiag[row+col]=1;
+                upperdiag[n-1+col-row]=1;
+                recur(col+1,ans,board,leftrow,lowerdiag,upperdiag,n);
                 board[row][col]='.';
+                leftrow[row]=0;
+                lowerdiag[row+col]=0;
+                upperdiag[n-1+col-row]=0;
             }
         }
     }
@@ -43,7 +26,10 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        recur(0,ans,board,n);
+        vector<int>leftrow(n,0);
+        vector<int>lowerdiag(2*n-1,0);
+        vector<int>upperdiag(2*n-1,0);
+        recur(0,ans,board,leftrow,lowerdiag,upperdiag,n);
         return ans;
     }
 };
