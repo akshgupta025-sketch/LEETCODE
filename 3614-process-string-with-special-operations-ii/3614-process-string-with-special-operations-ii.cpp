dde74d6288f68c1b842c4f080c9a5a8ed9d85951
Akshat Gupta <akshgupta025@gmail.com>
2026-06-11 12:37:58 +0530

    Time: 24 ms (74.67%), Space: 23.9 MB (73.33%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        long long len = 0;
+        for(char ch:s){
+            if(ch>='a' && ch<='z'){
+                len++;
+            }else if(ch=='*'){
+                if(len>0)len--;
+            }else if(ch=='#'){
+                len=len*2;
+            }
+        }
+        long long n = s.size();
+        if(k>=len)return '.';
+        for(int i=n-1;i>=0;i--){
+            char ch = s[i];
+            if(ch>='a' && ch<='z'){
+                if(k==len-1)return ch;
+                len--;
+            }else if(ch=='*'){
+                len++;
+            }else if(ch=='#'){
+                long long half = len/2;
+                if(k>=half)k-=half;
+                len = half;
+            }else if(ch=='%'){
+                k = len-1-k;
+            }
+        }
+        return '.';
+        }
+};
