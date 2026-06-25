03ebb4502f8794349b166dc3b2c2c2ffac56d1ef
Akshat Gupta <akshgupta025@gmail.com>
2026-06-25 12:59:38 +0530

    Time: 62 ms (17.86%), Space: 41.3 MB (27.67%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -3,15 +3,27 @@ public:
 using ll = long long;
     int countMajoritySubarrays(vector<int>& nums, int target) {
         ll n = nums.size();
+          // BRUTE FORCE
         ll ans = 0;
-        for(int i=0;i<n;i++){
-            int cnt = 0;
-            for(int j=i;j<n;j++){
-                if(nums[j]==target){
-                    cnt++;
-                }
-                if(cnt>(j-i+1)/2)ans++;
-            }
+    //     for(int i=0;i<n;i++){
+    //         int cnt = 0;
+    //         for(int j=i;j<n;j++){
+    //             if(nums[j]==target){
+    //                 cnt++;
+    //             }
+    //             if(cnt>(j-i+1)/2)ans++;
+    //         }
+    // }
+  
+    // return ans;
+    vector<int>prefix(n+1,0);
+    for(int i=0;i<n;i++){
+        prefix[i+1] = prefix[i]+(nums[i]==target?1:-1);
+    }
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(prefix[j+1]-prefix[i]>0)ans++;
+        }
     }
     return ans;
     }
