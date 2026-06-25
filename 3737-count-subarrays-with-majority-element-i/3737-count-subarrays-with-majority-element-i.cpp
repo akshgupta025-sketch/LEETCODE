fcbd1828837f615fdb289298f7a16f841ed7d959
Akshat Gupta <akshgupta025@gmail.com>
2026-06-25 12:51:48 +0530

    Time: 59 ms (21.13%), Space: 39.6 MB (75.6%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -5,21 +5,14 @@ using ll = long long;
         ll n = nums.size();
         ll ans = 0;
         for(int i=0;i<n;i++){
-            int j = n-1;
             int cnt = 0;
-            for(int k=i;k<=j;k++){
-                if(nums[k]==target){
+            for(int j=i;j<n;j++){
+                if(nums[j]==target){
                     cnt++;
                 }
+                if(cnt>(j-i+1)/2)ans++;
             }
-            while(i<=j){
-             if(cnt>(j-i+1)/2)ans++;
-             if(nums[j]==target){
-                cnt--;
-             }
-             j--;
-            }
-        }
-        return ans;
+    }
+    return ans;
     }
 };
