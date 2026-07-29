88f51e6c362b70480c56f21408dd9dc7e8a41653
Akshat Gupta <akshgupta025@gmail.com>
2026-07-29 23:40:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (45.15%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -16,21 +16,46 @@ public:
         if(root==NULL){
             return ans;
         }
-      stack<TreeNode*>st1;
-      stack<TreeNode*>st2;
-      st1.push(root);
-      while(!st1.empty()){
-        TreeNode*y= st1.top();
-        st1.pop();
-        st2.push(y);
-        if(y->left!=NULL) st1.push(y->left);
-        if(y->right!=NULL) st1.push(y->right);
-      }
-      while(!st2.empty()){
-        TreeNode* x = st2.top();
-        st2.pop();
-        ans.push_back(x->val);
-      }
-      return ans;
+    //     // USING 2 STACKS 
+    //   stack<TreeNode*>st1;
+    //   stack<TreeNode*>st2;
+    //   st1.push(root);
+    //   while(!st1.empty()){
+    //     TreeNode*y= st1.top();
+    //     st1.pop();
+    //     st2.push(y);
+    //     if(y->left!=NULL) st1.push(y->left);
+    //     if(y->right!=NULL) st1.push(y->right);
+    //   }
+    //   while(!st2.empty()){
+    //     TreeNode* x = st2.top();
+    //     st2.pop();
+    //     ans.push_back(x->val);
+    //   }
+    //   return ans;
+    stack<TreeNode*>st;
+    TreeNode* cur = root;
+    while(cur!=NULL||!st.empty()){
+        if(cur!=NULL){
+            st.push(cur);
+            cur=cur->left;
+        }else{
+            TreeNode* temp = st.top()->right;
+            if(temp==NULL){
+                temp = st.top();
+                st.pop();
+                ans.push_back(temp->val);
+                while(!st.empty() && temp == st.top()->right){
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                }
+            }else{
+                cur = temp;
+            }
+        }
     }
+    return ans;
+    }
+    
 };
