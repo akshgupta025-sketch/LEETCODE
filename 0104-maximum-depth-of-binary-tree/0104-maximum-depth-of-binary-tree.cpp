7b6d8bf3463ad477e2174a154306a7e4e526770c
Akshat Gupta <akshgupta025@gmail.com>
2026-07-30 22:07:41 +0530

    Time: 0 ms (100%), Space: 22 MB (41.54%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,19 +13,17 @@ class Solution {
 public:
 void preorder(TreeNode* root, int depth , int &mdep){
     if(root==NULL){
-         depth--;
         mdep = max(mdep,depth);
         return;
     }
-    if(root!=NULL)depth++;
-    preorder(root->left,depth,mdep);
-    preorder(root->right,depth,mdep);
+    preorder(root->left,depth+1,mdep);
+    preorder(root->right,depth+1,mdep);
 }
     int maxDepth(TreeNode* root) {
         if(root==NULL)return 0;
         if(root->right==NULL && root->left == NULL)return 1;
-        int depth = 1;
-        int mdep = 1;
+        int depth = 0;
+        int mdep = 0;
         preorder(root,depth , mdep);
         return mdep;
     }
