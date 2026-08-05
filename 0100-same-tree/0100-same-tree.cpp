7fc6c0631b49c47d8f8df0745e3e101687d1db32
Akshat Gupta <akshgupta025@gmail.com>
2026-08-06 00:25:44 +0530

    Time: 0 ms (100%), Space: 13 MB (13.62%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,35 +11,38 @@
  */
 class Solution {
 public:
-void preorder(TreeNode* p,vector<int>& arr){
-    if(p==NULL){
-        arr.push_back(1e9);
-        return;
-    }
-    arr.push_back(p->val);
-    preorder(p->left,arr);
-    preorder(p->right,arr);
-}
+// void preorder(TreeNode* p,vector<int>& arr){
+//     if(p==NULL){
+//         arr.push_back(1e9);
+//         return;
+//     }
+//     arr.push_back(p->val);
+//     preorder(p->left,arr);
+//     preorder(p->right,arr);
+// }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int>arr1;
-        vector<int>arr2;
-        preorder(p,arr1);
-        preorder(q,arr2);
-        int x = 0;
-        int y = 0;
-        int k = 1;
-        if(arr1.size()!=arr2.size())return false;
-        while(x<arr1.size()&& y<arr2.size()){
-            if(arr1[x]!=arr2[y]){
-                k = 0;
-                break;
-            }
-            x++;
-            y++;
-        }
-        if(k==0){
-            return false;
-        }
-        return true;
+//         vector<int>arr1;
+//         vector<int>arr2;
+//         preorder(p,arr1);
+//         preorder(q,arr2);
+//         int x = 0;
+//         int y = 0;
+//         int k = 1;
+//         if(arr1.size()!=arr2.size())return false;
+//         while(x<arr1.size()&& y<arr2.size()){
+//             if(arr1[x]!=arr2[y]){
+//                 k = 0;
+//                 break;
+//             }
+//             x++;
+//             y++;
+//         }
+//         if(k==0){
+//             return false;
+//         }
+//         return true;
+if(p==NULL && q==NULL)return true;
+if(p==NULL||q==NULL)return false;
+return(p->val==q->val)&&isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
 };
