7aa9ec19efa1812b912daf8dd9293ad9bf0782fb
Akshat Gupta <akshgupta025@gmail.com>
2026-08-07 23:44:18 +0530

    Time: 2 ms (28.06%), Space: 52.3 MB (83.76%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int i = 0;
+        if(n==1)return true;
+        while(i<n-1){
+            int j = i+1;
+            int jump = nums[i]+i;;
+          int next = i;
+          int maxu = -1;
+          while(j<=min(jump,n-1)){
+            if(j+nums[j]>=maxu){
+                maxu=j+nums[j];
+               next = j;
+            }
+            j++;
+          }
+            if(n-1-i<=nums[i])return true;
+         if(next==i)return false;
+         i=next;
+                
+        }
+        return false;
+    }
+};
