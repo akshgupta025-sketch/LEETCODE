92524d319b794c26207e34a6911c99fd15868db9
Akshat Gupta <akshgupta025@gmail.com>
2026-08-27 23:02:17 +0530

    Time: 0 ms (100%), Space: 9.8 MB (90.45%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -9,30 +9,32 @@ public:
         for(char c:target){
             freq[c-'a']--;
         }
+        //store freq fir target k char ki freq minus taki remaining paat lage
         for(int i=target.size()-1;i>=0;i--){
             int cur = target[i]-'a';
             freq[cur]++;
-        bool ok = true;
+        bool ok = true; //har target k char me iterate karo piche se kyu taki apne ko smallest lexo mil jaye
         for(int c=0;c<26;c++){
              if(freq[c]<0){
                 ok = false;
                 break;
              }
+             //ab dekha ki agar kisi me ok = true h matlab ha yeh difference mil gya vrna negative ho jata koi
         }
         if(!ok) continue;
         int next = -1;
         for(int c=cur+1;c<26;c++){
             if(freq[c]>0){
                 next = c;
-                break;
+                break; //ab main logic lexo smallest prefix vahi difference element m next available greater element taki lexo ho jaye baki bachi string sort
             }
         }
-        if(next==-1)continue;
-        freq[next]--;
-        string ans = target.substr(0,i);
-        ans+=char('a'+next);
+        if(next==-1)continue; //let say koi bada elem nhi mila to possible nhi
+        freq[next]--; //freq kam ho gyi used
+        string ans = target.substr(0,i); //ye prefix liya
+        ans+=char('a'+next); //us index vale char ko dala
         for(int c=0;c<26;c++){
-            ans.append(freq[c],char('a'+c));
+            ans.append(freq[c],char('a'+c));  //baki string sort krdi
         }
         return ans;
     }
